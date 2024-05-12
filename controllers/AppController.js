0fa98ff89130fcf3075f237e05a5c7ff563b9dec
alghurabi0 <zaid.alghurabi01@gmail.com>
2024-05-12 03:30:25 +0300

    async and try catch

--- controllers/AppController.js
+++ controllers/AppController.js
@@ -1,26 +1,22 @@
 import redisClient from '../utils/redis';
 import dbClient from '../utils/db';
 
-class AppController {
-  static getStatus(req, res) {
-    try {
-      const redis = redisClient.isAlive();
-      const db = dbClient.isAlive();
-      res.status(200).send({ redis, db });
-    } catch (error) {
-      console.log(error);
-    }
-  }
-
-  static async getStats(req, res) {
-    try {
-      const users = await dbClient.nbUsers();
-      const files = await dbClient.nbFiles();
-      res.status(200).send({ users, files });
-    } catch (error) {
-      console.log(error);
-    }
+export function getStatus(req, res) {
+  try {
+    const redis = redisClient.isAlive();
+    const db = dbClient.isAlive();
+    res.status(200).send({ redis, db });
+  } catch (error) {
+    console.log(error);
   }
 }
 
-export default AppController;
+export async function getStats(req, res) {
+  try {
+    const users = await dbClient.nbUsers();
+    const files = await dbClient.nbFiles();
+    res.status(200).send({ users, files });
+  } catch (error) {
+    console.log(error);
+  }
+}
