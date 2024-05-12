38c2bf9cc48a5b49b7a89ea4a5dad451d0d46248
alghurabi0 <zaid.alghurabi01@gmail.com>
2024-05-12 04:11:01 +0300

    default

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const AppController = require('../controllers/AppController');
-const UserController = require('../controllers/UsersController');
+const UsersController = require('../controllers/UsersController');
 
 const router = express.Router();
 
@@ -11,6 +11,6 @@ router.get('/stats', (req, res) => {
   AppController.getStats(req, res);
 });
 router.post('/users', (req, res) => {
-  UserController.postNew(req, res);
+  UsersController.default.postNew(req, res);
 });
 module.exports = router;
