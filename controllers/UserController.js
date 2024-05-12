3560be721469b4129da37edff41af008d7516916
alghurabi0 <zaid.alghurabi01@gmail.com>
2024-05-12 03:49:08 +0300

    hashed

--- controllers/UserController.js
+++ controllers/UserController.js
@@ -1,3 +1,4 @@
+import sha1 from 'sha1';
 import dbClient from '../utils/db';
 
 export default async function postNew(req, res) {
@@ -9,7 +10,8 @@ export default async function postNew(req, res) {
     if (userExists) {
       return res.status(400).send({ error: 'Already exist' });
     }
-    await dbClient.createUser(email, password);
+    const hashedPassword = sha1(password);
+    await dbClient.createUser(email, hashedPassword);
     const user = await dbClient.getUserByEmail(email);
     return res.status(201).send({ id: user._id, email: user.email });
   } catch (error) {
