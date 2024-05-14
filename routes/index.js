1dc07c841ce7084251bbdfefb230aca0a9c1ff30
alghurabi0 <zaid.alghurabi01@gmail.com>
2024-05-14 18:46:03 +0300

    get to put

--- routes/index.js
+++ routes/index.js
@@ -33,10 +33,10 @@ router.get('/files/:id', (req, res) => {
 router.get('/files', (req, res) => {
   FilesController.getIndex(req, res);
 });
-router.get('files/:id/publish', (req, res) => {
+router.put('files/:id/publish', (req, res) => {
   FilesController.putPublish(req, res);
 });
-router.get('files/:id/unpublish', (req, res) => {
+router.put('files/:id/unpublish', (req, res) => {
   FilesController.putUnpublish(req, res);
 });
 router.get('files/:id/data', (req, res) => {
